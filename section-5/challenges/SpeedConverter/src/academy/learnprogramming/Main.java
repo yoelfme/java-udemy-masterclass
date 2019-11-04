dd62661cc87b9c43175a13be0b4c1d3abd67f2c8
Yoel Monzon <yoelfme@hotmail.com>
2019-11-04 17:31:50 -0600

    [update] reorganizing folder structures

---
+++ section-5/challenges/SpeedConverter/src/academy/learnprogramming/Main.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
+}
